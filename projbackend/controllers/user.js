5a2cb03b58264ca63aa5ea4464092fb32a30a453
SarvY1999 <sarveshy193@gmail.com>
2022-05-15 18:52:47 +0530

    Adds user purchase list control and route

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById = (req, res, next, id)=>{
     User.findById(id).exec((err, user)=>{
@@ -12,6 +13,7 @@ exports.getUserById = (req, res, next, id)=>{
     })
 }
 
+
 exports.getUser = (req, res)=>{
     
     req.profile.salt= undefined;
@@ -19,4 +21,39 @@ exports.getUser = (req, res)=>{
     req.profile.createdAt = undefined;
     req.profile.updatedAt = undefined;
     return res.json(req.profile);
+}
+
+//updating user data
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(
+        {_id: req.profile._id},
+        {$set: req.body},
+        {new: true, useFindAndModify: false},
+        (err, user) => {
+            if(err){
+                return res.status(400).json({
+                    error: "You are not authorized to update this user"
+                })
+            }
+            user.salt= undefined;
+            user.encry_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+            res.json(user)
+        }
+    )
+}
+
+// user purchase list 
+// Pulling this information from order model 
+exports.userPurchaseList = (req, res) => {
+    Order.find({user: req.profile._id,})
+    .populate("user", "_id name")
+    .exec((err, order)=>{
+        if(err){
+            return res.status(400).json({
+                error: "No reder in this account"
+            })
+        }return res.json(order)
+    })
 }
