56f4584895f5f38de409987d673d9248e1c1e579
SarvY1999 <sarveshy193@gmail.com>
2022-05-15 20:10:32 +0530

    adding middleware pushOrderInPurchaseList

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -57,3 +57,33 @@ exports.userPurchaseList = (req, res) => {
         }return res.json(order)
     })
 }
+
+//Middleware
+exports.pushOrderInPurchaseList = (req, res, next) =>{
+    
+    let purchases = []
+    req.body.order.products.forEach(product =>{
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        })
+    })
+    
+    //store this in DB
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {purchases : purchases}},
+        {new:true},
+        (err, purchase)=>{
+            if(err){
+                return res.status(400).json({
+                    error: "Undable to save purchase list"
+                })
+            }next()
+        })
+}
