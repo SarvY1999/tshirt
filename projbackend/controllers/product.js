fb8182d5f13d3ffe6b9600ed935f1c6d2774a959
SarvY1999 <sarveshy193@gmail.com>
2022-05-22 00:31:25 +0530

    Adds updateStock, getAllUniqueCategories controllers and getAllUniqueCategories route

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -151,3 +151,33 @@ exports.getAllProducts =(req, res) => {
         }res.json(products)
     })
 }
+
+exports.updateStock = (req, res, next) => {
+    let myOperations = req.body.order.product.map(prod => {
+        return {
+            updateOne: {
+                filter: {_id: prod._id},
+                update: {$inc: {stock: -prod.count, sold: +prod.count}}
+            }
+        }
+    })
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if(err){
+            return res.status(400).json({
+                error: "Bulkwrite failed"
+            })
+        }next()
+    })
+}
+
+// for more explaination on getAllUniqueCategories refer to video no.11 from section 10
+exports.getAllUniqueCategories = (req, res) => {
+    Product.distinct("catrgory", {}, (err, category) => {
+        if(err){
+            return res.status(400).json({
+                error: "No Category found"
+            });
+        }
+        res.json(category)
+    })
+}
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct, getAllProducts} = require("../controllers/product");
+const {getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct, getAllProducts, getAllUniqueCategories} = require("../controllers/product");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
@@ -25,6 +25,8 @@ router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, 
 
 // listing route
 router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories); 
 
 
 module.exports = router
